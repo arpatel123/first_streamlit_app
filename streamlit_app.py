6200e085c688fceb0f03e9b3362942281b4b1eea
arpatel123 <138911595+arpatel123@users.noreply.github.com>
2023-07-08 00:00:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title('My Parents New healthy Dinner')
-streamlit.header('Breakfast Meenu')
-streamlit.text('Monday - Biled Egg')
+streamlit.title('🥗My Parents New healthy Dinner')
+streamlit.header('🥑 Breakfast Meenu')
+streamlit.text('🍞 Monday - Biled Egg')
 streamlit.text('Tuesday - Banana')
 streamlit.text('Wed - Fish')
 streamlit.text('Thus - Veg')
