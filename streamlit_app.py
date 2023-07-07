ead8040e5e016eaddcf4df5f2036b50ee14339fd
arpatel123 <138911595+arpatel123@users.noreply.github.com>
2023-07-08 00:07:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
-import streamlit
 streamlit.title('🥗My Parents New healthy Dinner')
 streamlit.header('🥑 Breakfast Meenu')
 streamlit.text('🍞 Monday - Biled Egg')
 streamlit.text('Tuesday - Banana')
 streamlit.text('Wed - Fish')
 streamlit.text('Thus - Veg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')                
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')     
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+           
